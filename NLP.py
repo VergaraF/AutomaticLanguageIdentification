927935eb8b57bf7f6957870ab87fff7e8e43abac
Camilo <Videscamilo@yahoo.com>
2018-11-29 17:39:45 -0500

    Added unigramEN, unigramFR and tested the 10 sentences, #4 and #10 fail

--- NLP.py
+++ NLP.py
@@ -1,14 +1,24 @@
+import math
 smoothing = 0.5
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+en_language_model = {}
+fr_language_model = {}
+es_language_model = {}
 
+
+# Function to check the probability of a character in a list of characters
 def probability(list, character):
     total_values = len(list)
     character_prob = 0;
     for i in list:
-        if character == i:
+        if character.lower() == i:
             character_prob+=1
-    actual_prob = (character_prob / total_values) + smoothing
+    actual_prob = ((character_prob + smoothing) / (total_values + smoothing * len(alphabet)))
     return actual_prob;
 
+
+# Function to check the conditional probability of a character in a list of characters
+# Given the character before it
 def conditional_probability(list, character, given_character):
     total_values = len(list)
     character_prob = 0;
@@ -17,29 +27,84 @@ def conditional_probability(list, character, given_character):
         if character == i and given_character == previous_char:
             character_prob+=1
         previous_char = i
-    actual_prob = (character_prob / total_values) + smoothing
+    actual_prob = (character_prob / total_values)
     return actual_prob;
 
+
+# Parse the file and make a list of all the characters in the file
 def uni_parse(fname):
     char_list = [ch for ch in open('./Data/'+ fname).read().lower() if ch != '\n' if ch != ' ' if ch != '"' if ch != ',' if ch != '!' if ch != '-' if ch != ':' if ch != ';' if ch != '.']
-    #print(char_list)
     return char_list
 
-def bi_parse(fname):
-    char_list = [ch for ch in open('./Data/'+ fname).read().lower() if ch != '\n' if ch != ' ' if ch != '"' if ch != ',' if ch != '!' if ch != '-' if ch != ':' if ch != ';' if ch != '.']
-    #print(char_list)
-    return char_list
 
-def unigram(model, sentence):
-    probability_so_far = 0.5;
+# Function that takes a language model and a sentence and figures out the sum of probabilities of the sentence
+def unigram_sentence(sentence):
+    probability_so_far_en = 0;
+    probability_so_far_fr = 0;
+    # probability_so_far_es = 0;
+    print(sentence)
     for ch in sentence:
-        if ch != '\n' and ch != ' ' and ch != '"' and ch != ',' and ch != '!' and ch != '-' and ch != ':' and ch != ';' and ch != '.' and ch != '?':
-            probability_so_far *= probability(model, ch)
-            print("P("+ch+") = " + str(probability_so_far))
-    return probability_so_far
+        if ch != '\n' and ch != ' ' and ch != '"' and ch != ',' and ch != '!' and ch != '-' and ch != ':' and ch != ';' and ch != '.' and ch != '?' and ch != '’' and ch != '\'':
+            probability_so_far_en += (math.log(en_language_model[ch.lower()], 10))
+            probability_so_far_fr += (math.log(fr_language_model[ch.lower()], 10))
+            # probability_so_far_es += (math.log(es_language_model[ch.lower()] + smoothing, 10))
+
+            print("Unigram: " + ch.lower())
+            print("FRENCH:  " + 'P(' + ch.lower() + ') = ' + str(fr_language_model[ch.lower()]) + ' ==> log prob of sentence so far: ' + str(math.exp(probability_so_far_fr)))
+            print("ENGLISH: " + 'P(' + ch.lower() + ') = ' + str(en_language_model[ch.lower()]) + ' ==> log prob of sentence so far: ' + str(math.exp(probability_so_far_en)))
+            # print("SPANISH: " + 'P(' + ch.lower() + ') = ' + es_language_model[ch] + ' ==> log prob of sentence so far: ' + probability_so_far_FR)
+
+    #TODO: Return language accordning to probabilities
+    if probability_so_far_en > probability_so_far_fr:
+        return "\nAccording to the unigram model, the sentence is in English "
+    elif probability_so_far_en < probability_so_far_fr:
+        return "\nAccording to the unigram model, the sentence is in French "
+    else:
+        return "\nAccording to the unigram model, the sentence is in Spanish "
+
+    return probability_so_far_en
 
 
+# Function that will output the lamguage model and their probabilities for a unigram and a language
+# Also returns the model after training
+def train_unigram_model(model, language):
+    language_model_output = ''
 
-uni_model = uni_parse("en-the-little-prince.txt")
-final_prob = unigram(uni_model,"What will the Japanese economy be like next year?")
-print(final_prob)
+    for character in alphabet:
+        if language == 'EN':
+            en_language_model.update({character.lower(): probability(model, character)})
+        elif language == 'FR':
+            fr_language_model.update({character.lower(): probability(model, character)})
+        elif language == 'ES':
+            es_language_model.update({character.lower(): probability(model, character)})
+        else:
+            language_model_output += "ERROR"
+        language_model_output +=  ("P(" + character + ") = " + str(probability(model, character))) + '\n'
+
+    f = open("unigram" +language+ ".txt", "w")
+    f.write(language_model_output)
+
+    # TODO: need to be written to a file for every language
+    if language == 'EN':
+        #print(en_language_model)
+        return en_language_model
+    elif language == 'FR':
+        print(fr_language_model)
+        return fr_language_model
+    elif language == 'ES':
+        #print(es_language_model)
+        return es_language_model
+
+
+# First Attempt to Train an english model and predict if it's english.
+training_corpora_en = uni_parse("en-the-little-prince.txt")
+training_corpora_fr = uni_parse("fr-le-petit-prince.txt")
+# training_corpora_es = uni_parse("el-principito.txt")
+
+# Language Models
+english_model = train_unigram_model(training_corpora_en, 'EN')
+french_model = train_unigram_model(training_corpora_fr, 'FR')
+# spanish_model = train_unigram_model(training_corpora_es, 'ES')
+
+language = unigram_sentence("What will the Japanese economy be like next year?")
+print(language)
